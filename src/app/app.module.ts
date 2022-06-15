8cb3e33f9c24b641d7f526af25235cc72d9a2ecb
martintoledoo <martinnfcn@gmail.com>
2022-06-15 19:01:16 -0300

    pymes2022

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,49 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS} from "@angular/common/http";
+import { RouterModule } from '@angular/router';
+import {APP_BASE_HREF} from '@angular/common';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { ArticulosFamiliasComponent } from './components/articulos-familias/articulos-familias.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { ArticulosComponent } from './components/articulos/articulos.component';
+
+import { ReactiveFormsModule } from "@angular/forms";
+import {NgbPaginationModule, NgbModalModule} from "@ng-bootstrap/ng-bootstrap";
+import { ModalDialogComponent } from './components/modal-dialog/modal-dialog.component';
+import { MyInterceptor } from "./shared/my-interceptor/my-interceptor.component";
+
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent,
+    InicioComponent,
+    ArticulosFamiliasComponent,
+    MenuComponent,
+    ArticulosComponent,
+    ModalDialogComponent,
+    
+  ],
+  imports: [
+    HttpClientModule,
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', redirectTo: '/inicio', pathMatch: 'full' },
+      { path: 'inicio', component: InicioComponent },
+      { path: 'articulosfamilias', component: ArticulosFamiliasComponent },
+      { path: 'articulos', component: ArticulosComponent }
+      ]),
+      ReactiveFormsModule,
+      NgbPaginationModule,
+      NgbModalModule
+  ],
+  providers: [
+    { provide: APP_BASE_HREF, useValue: "/" },
+    { provide: HTTP_INTERCEPTORS, useClass: MyInterceptor, multi: true }
+  ],
+  bootstrap: [AppComponent],
+  entryComponents: [ModalDialogComponent],
 })
 export class AppModule { }
